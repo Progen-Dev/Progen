074707b184fafabfcba26ee4307cd0529c91e183
mtorials <matti.bleckmann@live.com>
2019-07-18 12:41:34 +0200

    Added curde WarnBoard command

--- src/main/java/de/progen_bot/commands/WarnBoard.java
+++ src/main/java/de/progen_bot/commands/WarnBoard.java
@@ -1,9 +1,14 @@
 package de.progen_bot.commands;
 
+import de.mtorials.models.Warn;
 import de.progen_bot.command.CommandHandler;
 import de.progen_bot.command.CommandManager;
 import de.progen_bot.core.Main;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class WarnBoard extends CommandHandler {
 
@@ -14,7 +19,16 @@ public class WarnBoard extends CommandHandler {
     @Override
     public void execute(CommandManager.ParsedCommandString parsedCommand, MessageReceivedEvent event) {
 
+        StringBuilder msg = new StringBuilder();
 
+        HashMap<Member, ArrayList<Warn>> x = Main.getDAOWarnList().getWarnsByMembersForGuild(event.getGuild());
+
+        for (Member m : x.keySet()) {
+
+            msg.append(m.getNickname()).append(": ").append(x.get(m).size()).append("\n");
+        }
+
+        event.getMessage().getTextChannel().sendMessage(msg);
     }
 
     @Override
