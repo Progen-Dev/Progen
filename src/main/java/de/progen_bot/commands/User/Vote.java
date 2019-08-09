54d4209cdb9223274b7f515c0bdcc838d16cac55
mtorials <matti.bleckmann@live.com>
2019-08-09 11:21:02 +0200

    Added vote

---
+++ src/main/java/de/progen_bot/commands/User/Vote.java
@@ -0,0 +1,28 @@
+package de.progen_bot.commands.User;
+
+import de.mtorials.config.GuildConfiguration;
+import de.progen_bot.command.CommandHandler;
+import de.progen_bot.command.CommandManager;
+import de.progen_bot.db.UserData;
+import de.progen_bot.util.Statics;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Vote extends CommandHandler {
+
+    public Vote() {
+        super("vote", "vote", "Get the vote link to support Progen and earn XP!");
+    }
+
+    @Override
+    public void execute(CommandManager.ParsedCommandString parsedCommand, MessageReceivedEvent event, GuildConfiguration configuration) {
+
+        event.getAuthor().openPrivateChannel().complete().sendMessage("Thank you for your support. Here is the link:\n https://discordbots.org/bot/495293590503817237").queue();
+        event.getTextChannel().sendMessage("Thank you for voting " + event.getMember().getEffectiveName() + ". You can also vote and earn XP!").queue();
+
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+}
--- src/main/java/de/progen_bot/util/Statics.java
+++ src/main/java/de/progen_bot/util/Statics.java
@@ -2,4 +2,5 @@ package de.progen_bot.util;
 
 public class Statics {
 	public static int XP_GAIN = 2;
+	public static int XP_GAIN_VOTE = 5;
 }
