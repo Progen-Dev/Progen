a95d63c8ff32244014dfde461193b85c5d26412a
byNoobiYT <bynoobiyt@gmail.com>
2020-03-23 13:31:10 +0100

    private contructors, null cheks

--- src/main/java/de/progen_bot/db/connection/ConnectionFactory.java
+++ src/main/java/de/progen_bot/db/connection/ConnectionFactory.java
@@ -1,14 +1,14 @@
 package de.progen_bot.db.connection;
 
-import com.mysql.cj.jdbc.Driver;
 import de.progen_bot.core.Main;
-import de.progen_bot.util.Settings;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class ConnectionFactory {
+
+    private ConnectionFactory() {
+        /* Prevent instantiation */
+    }
 
     /**
      * Get a connection to database
