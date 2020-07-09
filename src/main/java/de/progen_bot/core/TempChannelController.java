17d90ef376bf8b37b130e8dbc32d1df409647189
byNoobiYT <bynoobiyt@gmail.com>
2020-07-09 11:23:06 +0200

    Format classes

--- src/main/java/de/progen_bot/core/TempChannelController.java
+++ src/main/java/de/progen_bot/core/TempChannelController.java
@@ -1,7 +1,6 @@
 package de.progen_bot.core;
 
 import net.dv8tion.jda.api.entities.Category;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 
 public class TempChannelController {
@@ -14,16 +13,11 @@ public class TempChannelController {
     }
 
     public void createTextChannel(String name, int userlimit) {
-
-        TextChannel channel = category.createTextChannel(name).complete();
-        channel.getManager().setParent(category).queue();
-        channel.getManager().setUserLimit(userlimit).queue();
+        category.createTextChannel(name).setParent(category).setUserlimit(userlimit).queue();
 	}
 
     public void createTextChannel(String name) {
-
-        TextChannel channel = category.createTextChannel(name).complete();
-        channel.getManager().setParent(category).queue();
+        category.createTextChannel(name).setParent(category).queue();
 	}
 
 
