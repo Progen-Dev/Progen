07d146678e18159872cc7403cc2895b72467d9f5
byNoobiYT <bynoobiyt@gmail.com>
2020-07-14 12:43:59 +0200

    error handling

--- src/main/java/de/progen_bot/listeners/MuteHandler.java
+++ src/main/java/de/progen_bot/listeners/MuteHandler.java
@@ -8,19 +8,26 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.awt.*;
 
-public class MuteHandler extends ListenerAdapter {
+public class MuteHandler extends ListenerAdapter
+{
     @Override
-    public void onMessageReceived(MessageReceivedEvent e) {
+    public void onMessageReceived(MessageReceivedEvent e)
+    {
         final User sender = e.getAuthor();
         final MuteData data = MuteData.getMuteData(sender.getId());
-        if (data != null && data.getGuildId().equals(e.getGuild().getId())) {
+        if (data != null && data.getGuildId().equals(e.getGuild().getId()))
+        {
             sender.openPrivateChannel().queue(pc -> pc.sendMessage(
                     new EmbedBuilder().setColor(Color.orange).setDescription(
                             "You can not write on this server, because you are muted in text channels!\n" +
                                     "Please contact an Supporter, Moderator or Admin at `" + e.getGuild().getName() + "` to unmute.\n\n" +
                                     "Mute Reason: `" + data.getReason() + "`"
                     ).build()
-            ).queue());
+            ).queue(), err ->
+            {
+                System.err.println("Failed to send message to user\n");
+                err.printStackTrace();
+            });
             e.getMessage().delete().queue();
         }
     }
