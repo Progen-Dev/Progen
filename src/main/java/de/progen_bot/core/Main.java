2b85d89770643fb7f60544e8a514d3cacf9f6ba2
Hove Gotthard <hgotthard@isogon.de>
2020-10-11 18:57:16 +0200

    adds timeout after 1 day and fixes circle emote

--- src/main/java/de/progen_bot/core/Main.java
+++ src/main/java/de/progen_bot/core/Main.java
@@ -146,11 +146,14 @@ public class Main {
         final JDABuilder 
         builder = JDABuilder.createDefault(
             Settings.TOKEN,
-            GatewayIntent.GUILD_BANS,
+            GatewayIntent.GUILD_BANS,            
             GatewayIntent.GUILD_MESSAGES,
             GatewayIntent.GUILD_MEMBERS,
             GatewayIntent.GUILD_VOICE_STATES,
-            GatewayIntent.GUILD_PRESENCES);
+            GatewayIntent.GUILD_PRESENCES,
+            GatewayIntent.GUILD_MESSAGE_REACTIONS,
+            GatewayIntent.DIRECT_MESSAGE_REACTIONS,
+            GatewayIntent.DIRECT_MESSAGES);
         BuildManager.addEventListeners(builder);
         try {
             jda = builder.build().awaitReady();
