35a3b3a168c67b4c23181cf268c3be8b718d3930
Hove Gotthard <hgotthard@isogon.de>
2020-10-12 18:24:36 +0200

    removes entity from map and thread blocking

--- src/main/java/de/progen_bot/listeners/StarBoardListener.java
+++ src/main/java/de/progen_bot/listeners/StarBoardListener.java
@@ -11,14 +11,13 @@ import java.util.concurrent.TimeUnit;
 
 import de.progen_bot.core.Main;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class StarBoardListener extends ListenerAdapter {
-    private final Map<String, Message> starMessage = new HashMap<>();
+    private final Map<String, String> starMessage = new HashMap<>();
     private final Map<String, Integer> starCount = new HashMap<>();
 
     final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
@@ -41,35 +40,42 @@ public class StarBoardListener extends ListenerAdapter {
 
         if (event.getReactionEmote().getName().equals("\u2B50")) {
 
-            Message mMessage = event.getJDA().getTextChannelById(event.getChannel().getId())
-                    .retrieveMessageById(event.getMessageId()).complete();
+            event.getJDA().getTextChannelById(event.getChannel().getId()).retrieveMessageById(event.getMessageId())
+                    .queue(mMessage -> {
 
-            mMessage.getReactions().forEach(reaction -> {
-                if (reaction.getReactionEmote().getName().equals("\u2B50")) {
+                        mMessage.getReactions().forEach(reaction -> {
+                            if (reaction.getReactionEmote().getName().equals("\u2B50")) {
 
-                    if (!starMessage.containsKey(mMessage.getGuild().getId())) {
-                        starMessage.put(mMessage.getGuild().getId(), mMessage);
-                        starCount.put(mMessage.getGuild().getId(), reaction.getCount());
-                    } else if (starCount.get(mMessage.getGuild().getId()) < reaction.getCount()) {
-                        starMessage.put(mMessage.getGuild().getId(), mMessage);
-                        starCount.put(mMessage.getGuild().getId(), reaction.getCount());
-                    }
-                }
-            });
+                                if (!starMessage.containsKey(mMessage.getGuild().getId())) {
+                                    starMessage.put(mMessage.getGuild().getId(), mMessage.getId());
+                                    starCount.put(mMessage.getGuild().getId(), reaction.getCount());
+                                } else if (starCount.get(mMessage.getGuild().getId()) < reaction.getCount()) {
+                                    starMessage.put(mMessage.getGuild().getId(), mMessage.getId());
+                                    starCount.put(mMessage.getGuild().getId(), reaction.getCount());
+                                }
+                            }
+                        });
+                    });
         }
     }
 
-    private void printResult(String guildId, Message message, int count) {
-        final MessageEmbed embed = new EmbedBuilder()
-                .setFooter(message.getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME)).setColor(Color.YELLOW)
-                .setAuthor(message.getAuthor().getAsTag(), null, message.getAuthor().getAvatarUrl())
-                .setTitle("Jump to message", message.getJumpUrl()).setDescription(message.getContentDisplay()).build();
+    private void printResult(String guildId, String messageId, int count) {
+        Main.getJda().getGuildById(guildId).getTextChannels().forEach(channel -> {
+            channel.retrieveMessageById(messageId).queue(message -> {
+                final MessageEmbed embed = new EmbedBuilder()
+                        .setFooter(message.getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME))
+                        .setColor(Color.YELLOW)
+                        .setAuthor(message.getAuthor().getAsTag(), null, message.getAuthor().getAvatarUrl())
+                        .setTitle("Jump to message", message.getJumpUrl()).setDescription(message.getContentDisplay())
+                        .build();
 
-        final List<TextChannel> channelList = Main.getJda().getGuildById(guildId).getTextChannelsByName("starboard",
-                true);
+                final List<TextChannel> channelList = Main.getJda().getGuildById(guildId)
+                        .getTextChannelsByName("starboard", true);
 
-        if (!channelList.isEmpty()) {
-            channelList.get(0).sendMessage(count + " \u2B50").embed(embed).queue();
-        }
+                if (!channelList.isEmpty()) {
+                    channelList.get(0).sendMessage(count + " \u2B50").embed(embed).queue();
+                }
+            });
+        });
     }
 }
