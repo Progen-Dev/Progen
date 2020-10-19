5868635c093e481fe7fcdfec03970e02a1cc428b
Hove Gotthard <hgotthard@isogon.de>
2020-10-19 22:00:45 +0200

    handles error when PMs are disabled

--- src/main/java/de/progen_bot/commands/user/Say.java
+++ src/main/java/de/progen_bot/commands/user/Say.java
@@ -10,6 +10,7 @@ import de.progen_bot.listeners.SayModel;
 import de.progen_bot.permissions.AccessLevel;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.requests.ErrorResponse;
 
 public class Say extends CommandHandler {
 
@@ -21,14 +22,17 @@ public class Say extends CommandHandler {
     public void execute(ParsedCommandString parsedCommand, MessageReceivedEvent event,
             GuildConfiguration configuration) {
 
-        final String out = String.join(" ", parsedCommand.getArgs());
-
-        final SayModel sayModel = new SayModel(event.getTextChannel().getId(), new EmbedBuilder().setDescription(out)
-                .setAuthor(event.getAuthor().getAsTag()).setTimestamp(Instant.now()));
-        SayListener.addSayModel(event.getAuthor().getId(), sayModel);
-
         event.getAuthor().openPrivateChannel()
-                .queue(channel -> channel.sendMessage("Please choose a color in hex <#fff> or <#ffffff>").queue());
+                .flatMap(channel -> channel.sendMessage("Please choose a color in hex <#fff> or <#ffffff>"))
+                .onErrorFlatMap(ErrorResponse.CANNOT_SEND_TO_USER::test,
+                        (error) -> event.getTextChannel().sendMessage("Please enable your DM´s to use this command."))
+                .queue(v -> {
+                    final String out = String.join(" ", parsedCommand.getArgs());
+
+                    final SayModel sayModel = new SayModel(event.getTextChannel().getId(), new EmbedBuilder()
+                            .setDescription(out).setAuthor(event.getAuthor().getAsTag()).setTimestamp(Instant.now()));
+                    SayListener.addSayModel(event.getAuthor().getId(), sayModel);
+                });
     }
 
     @Override
