1e32cefd20a9a3537b55f94810a2d85e864a6681
ShowMeYourSkil <39274150+ShowMeYourSkil@users.noreply.github.com>
2020-10-19 18:53:20 +0200

    Version update

--- src/main/java/de/progen_bot/commands/owner/CommandUpdate.java
+++ src/main/java/de/progen_bot/commands/owner/CommandUpdate.java
@@ -22,7 +22,7 @@ public class CommandUpdate extends CommandHandler {
             new EmbedBuilder()
             .setColor(Color.blue)
             .setTitle("Update")
-            .setDescription("Hey @everyone, `1.0.8` is released.\n" + "https://github.com/Progen-Dev/Progen/releases/")
+            .setDescription("Hey @everyone, `" + Statics.VERSION + "is released.\n" + "https://github.com/Progen-Dev/Progen/releases/")
             .addField("Version", Statics.VERSION , false)
             .setTimestamp(Instant.now())
          .build()
