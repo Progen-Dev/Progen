aa8108ec9bf17235b19c7963d806ff0491aa62d1
Hove Gotthard <hgotthard@isogon.de>
2020-10-19 15:36:34 +0200

    fixes message not found errors adds a starboard object to save the messageId and channelId

--- src/main/java/de/progen_bot/listeners/StarBoardListener.java
+++ src/main/java/de/progen_bot/listeners/StarBoardListener.java
@@ -10,6 +10,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import de.progen_bot.core.Main;
+import de.progen_bot.util.StarBoard;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -17,15 +18,23 @@ import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEve
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class StarBoardListener extends ListenerAdapter {
-    private final Map<String, String> starMessage = new HashMap<>();
+
+    /**
+     * A HashMap with the key GuildId and value Starboard(MessageId, TextChannelId).
+     */
+    private final Map<String, StarBoard> starMessage = new HashMap<>();
+
+    /**
+     * A HashMap with the key GuildId and value the number of stars given.
+     */
     private final Map<String, Integer> starCount = new HashMap<>();
 
     final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public StarBoardListener() {
         scheduler.scheduleAtFixedRate(() -> {
-            starMessage.forEach((k, v) -> {
-                printResult(k, v, starCount.get(k));
+            starMessage.forEach((guildId, starBoard) -> {
+                printResult(guildId, starBoard, starCount.get(guildId));
             });
             starMessage.clear();
             starCount.clear();
@@ -34,20 +43,18 @@ public class StarBoardListener extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
-
         if (event.getReactionEmote().getName().equals("\u2B50")) {
-
             event.getJDA().getTextChannelById(event.getChannel().getId()).retrieveMessageById(event.getMessageId())
                     .queue(mMessage -> {
-
                         mMessage.getReactions().forEach(reaction -> {
                             if (reaction.getReactionEmote().getName().equals("\u2B50")) {
-
+                                StarBoard starBoard = new StarBoard(mMessage.getId(),
+                                        mMessage.getTextChannel().getId());
                                 if (!starMessage.containsKey(mMessage.getGuild().getId())) {
-                                    starMessage.put(mMessage.getGuild().getId(), mMessage.getId());
+                                    starMessage.put(mMessage.getGuild().getId(), starBoard);
                                     starCount.put(mMessage.getGuild().getId(), reaction.getCount());
                                 } else if (starCount.get(mMessage.getGuild().getId()) < reaction.getCount()) {
-                                    starMessage.put(mMessage.getGuild().getId(), mMessage.getId());
+                                    starMessage.put(mMessage.getGuild().getId(), starBoard);
                                     starCount.put(mMessage.getGuild().getId(), reaction.getCount());
                                 }
                             }
@@ -56,23 +63,22 @@ public class StarBoardListener extends ListenerAdapter {
         }
     }
 
-    private void printResult(String guildId, String messageId, int count) {
-        Main.getJda().getGuildById(guildId).getTextChannels().forEach(channel -> {
-            channel.retrieveMessageById(messageId).queue(message -> {
-                final MessageEmbed embed = new EmbedBuilder()
-                        .setFooter(message.getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME))
-                        .setColor(Color.YELLOW)
-                        .setAuthor(message.getAuthor().getAsTag(), null, message.getAuthor().getAvatarUrl())
-                        .setTitle("Jump to message", message.getJumpUrl()).setDescription(message.getContentDisplay())
-                        .build();
+    private void printResult(String guildId, StarBoard starBoard, int count) {
+        Main.getJda().getTextChannelById(starBoard.getTextchannelId()).retrieveMessageById(starBoard.getMessageId())
+                .queue(message -> {
+                    final MessageEmbed embed = new EmbedBuilder()
+                            .setFooter(message.getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME))
+                            .setColor(Color.YELLOW)
+                            .setAuthor(message.getAuthor().getAsTag(), null, message.getAuthor().getAvatarUrl())
+                            .setTitle("Jump to message", message.getJumpUrl())
+                            .setDescription(message.getContentDisplay()).build();
 
-                final List<TextChannel> channelList = Main.getJda().getGuildById(guildId)
-                        .getTextChannelsByName("starboard", true);
+                    final List<TextChannel> channelList = Main.getJda().getGuildById(guildId)
+                            .getTextChannelsByName("starboard", true);
 
-                if (!channelList.isEmpty()) {
-                    channelList.get(0).sendMessage(count + " \u2B50").embed(embed).queue();
-                }
-            });
-        });
+                    if (!channelList.isEmpty()) {
+                        channelList.get(0).sendMessage(count + " \u2B50").embed(embed).queue();
+                    }
+                });
     }
 }
