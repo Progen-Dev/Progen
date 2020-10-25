4fa731863751374b02046764340cccecd0d43bb3
ShowMeYourSkil <39274150+ShowMeYourSkil@users.noreply.github.com>
2020-10-25 17:13:18 +0100

    initialisation of required objects

--- src/main/java/de/mtorials/pwi/oauth/session/SessionManager.java
+++ src/main/java/de/mtorials/pwi/oauth/session/SessionManager.java
@@ -1,18 +1,32 @@
 package de.mtorials.pwi.oauth.session;
 
+import com.jagrosh.jdautilities.oauth2.Scope;
+import com.jagrosh.jdautilities.oauth2.session.SessionData;
+
 public class SessionManager{
-    /**
-     * Create a new Session
-     */
-    public void newSession(){}
 
-    /**
-     * Delete Season after a logout or timeout
-     */
-    public void deleteSession(){}
+	private final String sessionKey, accessToken, refreshToken, tokenType;
+	private final Scope[] scopes;
+	private String id;
 
-    /**
-     * Checks if the season of a user already exists.
-     */
-    public void checkSession(){}
+	public SessionManager(String userId, SessionData sessionData){
+		this.id = userId;
+		this.sessionKey = sessionData.getIdentifier();
+		this.accessToken = sessionData.getAccessToken();
+		this.refreshToken = sessionData.getRefreshToken();
+		this.tokenType = sessionData.getTokenType();
+		this.scopes = sessionData.getScopes();
+	}
+
+	public String getSessionKey(){
+		return this.sessionKey;
+	}
+
+	public String getUserId(){
+		return this.id;
+	}
+
+	public void setUserId(String userId){
+		this.id = userId;
+    }
 }
