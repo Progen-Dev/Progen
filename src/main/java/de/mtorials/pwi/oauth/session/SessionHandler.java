2d3df84b11e116b324e991e5aaa9eb8c91146fe4
ShowMeYourSkil <39274150+ShowMeYourSkil@users.noreply.github.com>
2020-10-25 15:19:51 +0100

    Import jdautilities.session

--- src/main/java/de/mtorials/pwi/oauth/session/SessionHandler.java
+++ src/main/java/de/mtorials/pwi/oauth/session/SessionHandler.java
@@ -2,6 +2,8 @@ package de.mtorials.pwi.oauth.session;
 
 import java.util.HashMap;
 
+import com.jagrosh.jdautilities.oauth2.session.Session;
+
 public class SessionHandler{
     private static HashMap<String, Session> SESSION;
     private static HashMap<String, Boolean> USER;
