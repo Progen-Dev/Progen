b9a4778acf1d159237572395180e9bbcbfca4484
ShowMeYourSkil <39274150+ShowMeYourSkil@users.noreply.github.com>
2020-10-25 14:45:39 +0100

    Add Class SessionHandler with HashMap

---
+++ src/main/java/de/mtorials/pwi/oauth/session/SessionHandler.java
@@ -0,0 +1,10 @@
+package de.mtorials.pwi.oauth.session;
+
+import java.util.HashMap;
+
+public class SessionHandler{
+    private static HashMap<String, Session> SESSION;
+    private static HashMap<String, Boolean> USER;
+
+    
+}
