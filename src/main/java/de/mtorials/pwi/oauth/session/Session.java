b5e33f5377dbbffe5b29f616ae2762a2dd6b28ca
ShowMeYourSkil <39274150+ShowMeYourSkil@users.noreply.github.com>
2020-10-25 14:46:46 +0100

    Add Session.java with three session methods

---
+++ src/main/java/de/mtorials/pwi/oauth/session/Session.java
@@ -0,0 +1,18 @@
+package de.mtorials.pwi.oauth.session;
+
+public class Session{
+    /**
+     * Create a new Session
+     */
+    public void newSession(){}
+
+    /**
+     * Delete Season after a logout or timeout
+     */
+    public void deleteSession(){}
+
+    /**
+     * Checks if the season of a user already exists.
+     */
+    public void checkSession(){}
+}
