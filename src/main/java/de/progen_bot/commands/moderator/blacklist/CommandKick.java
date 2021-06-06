626952c28256b4b16aaa69fd43b23ea223fc7828
ShowMeYourSkil <davidfranzen@t-online.de>
2021-06-06 18:33:49 +0200

    Grammar correction and little Test

--- src/main/java/de/progen_bot/commands/moderator/blacklist/CommandKick.java
+++ src/main/java/de/progen_bot/commands/moderator/blacklist/CommandKick.java
@@ -6,15 +6,18 @@ import de.progen_bot.db.entities.config.GuildConfiguration;
 import de.progen_bot.permissions.AccessLevel;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.guild.invite.GuildInviteCreateEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.requests.restaction.InviteAction;
 
+import javax.annotation.Nonnull;
 import java.awt.*;
 import java.time.Instant;
 import java.util.List;
 
 
 public class CommandKick extends CommandHandler {
-    private static final String KICK = "Kick";
+    private static final String KICK = "KICK";
     private static final String EXECUTOR = "Executor";
     private static final String VICTIM = "Victim";
     private static final String REASON = "Reason";
@@ -24,6 +27,9 @@ public class CommandKick extends CommandHandler {
         super("kick" , "kick <@user> <reason>" , "Kick a user from this server");
     }
 
+    private void onGuildInviteCreate(@Nonnull GuildInviteCreateEvent event) {
+        InviteAction test = event.getChannel().createInvite();
+    }
     private MessageEmbed getKickEmbed(MessageReceivedEvent event, String reason) {
         if (event.getMember() == null)
             return null;
@@ -34,11 +40,10 @@ public class CommandKick extends CommandHandler {
                 .addField(VICTIM, event.getMessage().getMentionedMembers().get(0).getAsMention(), true)
                 .addField(EXECUTOR, event.getMember().getAsMention(),true)
                 .setDescription(event.getMessageId())
-                .addField(REASON, reason, false)
                 .addField(CHANNEL, event.getTextChannel().getAsMention(), false)
+                .addField(REASON, reason, false)
                 .setTimestamp(Instant.now()).build();
     }
-
 
         @Override
         public void execute(CommandManager.ParsedCommandString parsedCommand , MessageReceivedEvent event , GuildConfiguration configuration) {
