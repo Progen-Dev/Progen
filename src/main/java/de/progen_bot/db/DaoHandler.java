f8c5cdbff57d08b32ef9983421cf7a79c210077b
ShowMeYourSkil <davidfranzen@t-online.de>
2021-07-05 21:27:00 +0200

    Implement the MessageDaoImpl

--- src/main/java/de/progen_bot/db/DaoHandler.java
+++ src/main/java/de/progen_bot/db/DaoHandler.java
@@ -3,6 +3,7 @@ package de.progen_bot.db;
 import java.util.ArrayList;
 
 import de.progen_bot.db.dao.Dao;
+import de.progen_bot.db.dao.Messages.MessageDaoImpl;
 import de.progen_bot.db.dao.config.ConfigDaoImpl;
 import de.progen_bot.db.dao.connectfour.ConnectFourDaoImpl;
 import de.progen_bot.db.dao.mute.MuteDao;
@@ -26,6 +27,7 @@ public class DaoHandler {
         daoList.add(new PlaylistDaoImpl());
         daoList.add(new PlaylistSongDaoImpl());
         daoList.add(new MuteDao());
+        daoList.add(new MessageDaoImpl());
 
         for (Dao dao : daoList) {
             dao.generateTables("");
