bfb57475190b5a0159671a39918705d359440537
ShowMeYourSkil <davidfranzen@t-online.de>
2021-07-27 10:43:44 +0200

    Rewrite ChannelDao and ChannelDaoImpl

--- src/main/java/de/progen_bot/db/dao/channel/ChannelDao.java
+++ src/main/java/de/progen_bot/db/dao/channel/ChannelDao.java
@@ -1,18 +1,16 @@
 package de.progen_bot.db.dao.channel;
 
-import de.progen_bot.db.entities.channel.ChannelData;
-import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.util.List;
 
 public interface ChannelDao {
-    void addLogChannelId(ChannelData channelData, Guild guild);
-    ChannelData loadLogChannelId(ChannelData channelData, Guild guild);
-    void deleteLogChannelId(ChannelData channelData, Guild guild);
 
-    void addStarBoardChannelId(ChannelData channelData, Guild guild);
-    ChannelData loadStarBoardChannelId(ChannelData channelData, Guild guild);
-    void deleteStarBoardChannelId(ChannelData channelData, Guild guild);
+    void addLogChannel(TextChannel channel);
+    List<String> loadLogChannel(TextChannel channel);
+    void deleteLogChannel(TextChannel channel);
 
-    void addVoiceLogChannelId(ChannelData channelData, Guild guild);
-    ChannelData loadVoiceLogChannelId(ChannelData channelData, Guild guild);
-    void deleteVoiceLogChannelId(ChannelData channelData, Guild guild);
+    void addStarBoardChannel(TextChannel channel);
+    List<String> loadStarBoardChannel(TextChannel channel);
+    void deleteStarBoardChannel(TextChannel channel);
 }
--- src/main/java/de/progen_bot/db/dao/channel/ChannelDaoImpl.java
+++ src/main/java/de/progen_bot/db/dao/channel/ChannelDaoImpl.java
@@ -1,77 +1,25 @@
 package de.progen_bot.db.dao.channel;
 
-import de.progen_bot.db.dao.Dao;
-import de.progen_bot.db.entities.channel.ChannelBuilder;
-import de.progen_bot.db.entities.channel.ChannelData;
-import net.dv8tion.jda.api.entities.Guild;
 import de.progen_bot.db.connection.ConnectionFactory;
+import de.progen_bot.db.dao.Dao;
+import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ChannelDaoImpl extends Dao implements ChannelDao{
+public class ChannelDaoImpl extends Dao implements ChannelDao {
     @Override
-    public void addLogChannelId(ChannelData channelData , Guild guild) {
-        Connection connection = ConnectionFactory.getConnection();
-
-        try {
-            PreparedStatement ps = connection.prepareStatement("REPLACE INTO `channels` (`guildId`, `logChannelId`"
-                    + ") VALUES (?,?);");
-            ps.setString(1, guild.getId());
-            ps.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public ChannelData loadLogChannelId(ChannelData channelData , Guild guild) {
-        Connection connection = ConnectionFactory.getConnection();
-
-        try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM `channels` WHERE `guildId` =? AND `logChannelId` =?;");
-            ps.setString(1, guild.getId());
-            ps.setString(2, guild.getTextChannels().get(0).getId());
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()){
-                return new ChannelBuilder()
-                        .setLogChannelId(rs.getString("logChannelId"))
-                        .build();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public void deleteLogChannelId(ChannelData channelData , Guild guild) {
+    public void addLogChannel(TextChannel channel) {
         Connection connection = ConnectionFactory.getConnection();
 
         try {
             PreparedStatement ps = connection.prepareStatement(
-                    "DELETE FROM `channels` WHERE `guildId` =? AND `logChannelId` =?;"
-            );
-            ps.setString(1, guild.getId());
-            ps.setString(2, guild.getTextChannels().get(0).getId());
-            ps.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Override
-    public void addStarBoardChannelId(ChannelData channelData , Guild guild) {
-        Connection connection = ConnectionFactory.getConnection();
-
-        try {
-            PreparedStatement ps = connection.prepareStatement("REPLACE INTO `channels` (`guildId`, `starBoardChannelId`"
-                    + ") VALUES (?,?);");
-            ps.setString(1, guild.getId());
-            ps.setString(2, guild.getTextChannels().get(0).getId());
+                    "INSERT INTO `costumeChannel` (guildId, logChannelId) VALUES(?,?)");
+            ps.setString(1 , channel.getGuild().getId());
+            ps.setString(2 , channel.getId());
             ps.execute();
         } catch (Exception e) {
             e.printStackTrace();
@@ -79,88 +27,84 @@ public class ChannelDaoImpl extends Dao implements ChannelDao{
     }
 
     @Override
-    public ChannelData loadStarBoardChannelId(ChannelData channelData , Guild guild) {
-        Connection connection = ConnectionFactory.getConnection();
-
-        try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM `channels` WHERE `guildId` =? AND `starBoardChannelId` =?;");
-            ps.setString(1, guild.getId());
-            ps.setString(2, guild.getTextChannels().get(0).getId());
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()){
-                return new ChannelBuilder()
-                        .setStarBoardChannelId(rs.getString("starBoardChannelId"))
-                        .build();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public void deleteStarBoardChannelId(ChannelData channelData , Guild guild) {
+    public List<String> loadLogChannel(TextChannel channel) {
+        List<String> logChannel = new ArrayList<>();
         Connection connection = ConnectionFactory.getConnection();
 
         try {
             PreparedStatement ps = connection.prepareStatement(
-                    "DELETE FROM `channels` WHERE `guildId` =? AND `starBoardChannelId` =?;"
+                    "SELECT * FROM `costumeChannel` WHERE `guildId` = ? AND `logChannelId` = ?"
             );
-            ps.setString(1, guild.getId());
-            ps.setString(2, guild.getTextChannels().get(0).getId());
-            ps.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+            ps.setString(1, channel.getGuild().getId());
+            ps.setString(2, channel.getId());
 
-    @Override
-    public void addVoiceLogChannelId(ChannelData channelData , Guild guild) {
-        Connection connection = ConnectionFactory.getConnection();
-
-        try {
-            PreparedStatement ps = connection.prepareStatement("REPLACE INTO `channels` (`guildId`, `voiceLogChannelId`"
-                    + ") VALUES (?,?);");
-            ps.setString(1, guild.getId());
-            ps.setString(2, guild.getTextChannels().get(0).getId());
-            ps.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public ChannelData loadVoiceLogChannelId(ChannelData channelData , Guild guild) {
-        Connection connection = ConnectionFactory.getConnection();
-
-        try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM `channels` WHERE `guildId` =? AND `voiceLogChannelId` =?;");
-            ps.setString(1, guild.getId());
-            ps.setString(2, guild.getTextChannels().get(0).getId());
             ResultSet rs = ps.executeQuery();
 
-            if (rs.next()){
-                return new ChannelBuilder()
-                        .setVoiceLogChannelId(rs.getString("voiceLogChannelId"))
-                        .build();
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return logChannel;
     }
 
     @Override
-    public void deleteVoiceLogChannelId(ChannelData channelData , Guild guild) {
+    public void deleteLogChannel(TextChannel channel) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement(
+                    "DELETE FROM `costumeChannel` WHERE `guildId` =? AND `logChannelId` = ?"
+            );
+            ps.setString(1, channel.getGuild().getId());
+            ps.setString(2, channel.getId());
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void addStarBoardChannel(TextChannel channel) {
         Connection connection = ConnectionFactory.getConnection();
 
         try {
             PreparedStatement ps = connection.prepareStatement(
-                    "DELETE FROM `channels` WHERE `guildId` =? AND `voiceLogChannelId` =?;"
+                    "INSERT INTO `costumeChannel` (guildId, starBordChannelId) VALUES(?,?)");
+            ps.setString(1, channel.getGuild().getId());
+            ps.setString(2, channel.getId());
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<String> loadStarBoardChannel(TextChannel channel) {
+        List<String> starBoardChannel = new ArrayList<>();
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(
+                    "SELECT * FROM `costumeChannel` WHERE `guildId` = ? AND `starBordChannelId` = ?"
             );
-            ps.setString(1, guild.getId());
-            ps.setString(2, guild.getTextChannels().get(0).getId());
+            ps.setString(1, channel.getGuild().getId());
+            ps.setString(2, channel.getId());
+
+            ResultSet rs = ps.executeQuery();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return starBoardChannel;
+    }
+
+    @Override
+    public void deleteStarBoardChannel(TextChannel channel) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement(
+                    "DELETE FROM `costumeChannel` WHERE `guildId` =? AND `starBordChannelId` = ?"
+            );
+            ps.setString(1, channel.getGuild().getId());
+            ps.setString(2, channel.getId());
             ps.execute();
         } catch (Exception e) {
             e.printStackTrace();
@@ -169,7 +113,10 @@ public class ChannelDaoImpl extends Dao implements ChannelDao{
 
     @Override
     public void generateTables(String query){
-        String sqlQuery = "CREATE TABLE IF NOT EXISTS channels (guildId VARCHAR(18) NOT NULL, logChannelId VARCHAR(18), starBoardChannelId VARCHAR(18), voiceLogChannelId VARCHAR(18)) ENGINE = InnoDB;";
+        String sqlQuery = "CREATE TABLE IF NOT EXISTS costumeChannel(`id` INT(11) NOT NULL AUTO_INCREMENT, `guildId` "
+                + "VARCHAR(50),`logChannelId` VARCHAR(50), "
+                + "`starBordChannelId` VARCHAR(50),"
+                + "PRIMARY KEY(`id`) ) ENGINE = InnoDB DEFAULT CHARSET = utf8";
         super.generateTables(sqlQuery);
     }
 }
